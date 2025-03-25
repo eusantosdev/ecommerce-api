a4144406432ebbdc481a9d522b542ccb8ab0537a
Alessandro <eusantosdev@gmail.com>
2025-03-24 21:17:59 -0300

    Métodos HTTPS, prática!

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
+let usuarios = [
+    { 
+        id: 1, 
+        nome: 'João', 
+        idade: 18 
+    }
+];
+
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
+app.get('/users', (req: Request, res: Response) => {
+    res.send(usuarios);
+});
+
+app.get('/users/:id', (req: Request, res: Response) => {
+    let id = req.params.id;
+    const userId = usuarios.filter(user => user.id === Number(id));
+    res.send(userId);
+});
+
 app.listen(3000, () => {
-    console.log("Servidor rodando!");
+    console.log('Servidor rodando!');
 });
