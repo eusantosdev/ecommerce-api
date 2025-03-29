1e142cef509461d9ac70481ffa036ca3adfe2775
Alessandro <eusantosdev@gmail.com>
2025-03-29 16:10:48 -0300

    Todos os métodos implementados!

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-let usuarios = [
-    { 
-        id: 1, 
-        nome: 'João', 
-        email: 'joao@gmail.com',
-        idade: 18 
-    }
-];
+app.use(express.json());
+
+type User = {
+    id: number,
+    nome: string,
+    email: string,
+    idade: number
+}
+
+let usuarios: User[] = [];
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
@@ -20,28 +22,40 @@ app.get('/users', (req: Request, res: Response) => {
 });
 
 app.get('/users/:id', (req: Request, res: Response) => {
-    let id = req.params.id;
+    let { id } = req.params
     const userId = usuarios.filter(user => user.id === Number(id));
     res.send(userId);
 });
 
 app.post('/users', (req: Request, res: Response) => {
-    let { nome, idade, email } = req.body;
-    const newUser = usuarios.push({
+    let { nome, email, idade } = req.body;
+    const newUser = {
         id: usuarios.length + 1,
         nome: nome,
         email: email,
         idade: idade
-    });
+    }
+    usuarios.push(newUser);
     res.send({
-        message: 'Novo usuario criado!',
-        newUser
+        message: `Usuario ${newUser.id} criado!`,
     });
 });
 
+app.put('/users/:id', (req: Request, res: Response) => {
+    let { id } = req.params;
+    let user = req.body;
+    let userIndex = usuarios.findIndex((_user: User) => user.id === Number(id));
+    usuarios[userIndex].nome = user.nome;
+    usuarios[userIndex].email = user.email;
+    usuarios[userIndex].idade = user.idade;
+    res.send({
+        message: `Dados de usuario ${id} atualizados!`
+    })
+})
+
 app.delete('/users/:id', (req: Request, res: Response) => {
-    let userId = req.params.id;
-    usuarios.filter(user => user.id !== Number(userId));
+    let { id } = req.params;
+    usuarios.filter(user => user.id !== Number(id));
     res.send({
         message: 'Usuario deletado!'
     });
