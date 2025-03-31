5960ed0b965b73d008c460f5f74b9cb8014d854e
Alessandro <eusantosdev@gmail.com>
2025-03-31 13:13:11 -0300

    Controllers adicionados!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { routes } from './routes/index.routes';
 
 const app = express();
 
 app.use(express.json());
+
+routes(app);
 
 app.listen((3000), () => {
     console.log('Servidor rodando!');
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,54 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { UserController } from "../controllers/users.controller";
 
 export const userRoutes = express.Router();
 
-type User = {
-    id: number,
-    nome: string,
-    email: string,
-};
-
-let usuarios: User[] = [];
-
-userRoutes.get('/users', (req: Request, res: Response) => {
-    res.send(usuarios);
-});
-
-userRoutes.get('/users/:id', (req: Request, res: Response) => {
-    let { id } = req.params
-    const userId = usuarios.filter(user => user.id === Number(id));
-    res.send(userId);
-});
-
-userRoutes.post('/users', (req: Request, res: Response) => {
-    let { nome, email } = req.body;
-    const newUser = {
-        id: usuarios.length + 1,
-        nome: nome,
-        email: email,
-    }
-    usuarios.push(newUser);
-    res.send({
-        message: `Usuario ${newUser.id} criado!`,
-    });
-});
-
-userRoutes.put('/users/:id', (req: Request, res: Response) => {
-    let { id } = req.params;
-    let user = req.body as User;
-    let indexOfUser = usuarios.findIndex((_user: User) => user.id === Number(id));
-    usuarios[indexOfUser].nome = user.nome;
-    usuarios[indexOfUser].email = user.email;
-    res.send({
-        message: `Dados de usuario ${id} atualizados!`
-    });
-});
-
-userRoutes.delete('/users/:id', (req: Request, res: Response) => {
-    let { id } = req.params;
-    let indexOfUser = usuarios.findIndex((_user: User) => _user.id === Number(id));
-    usuarios.splice(indexOfUser, 1);
-    res.send({
-        message: `Usuario ${id} deletado!`
-    });
-});
+userRoutes.get('/users', UserController.getAllUser);
+userRoutes.get('/users/:id', UserController.getUserById);
+userRoutes.post('/users', UserController.createUser);
+userRoutes.put('/users/:id', UserController.updateUser);
+userRoutes.delete('/users/:id', UserController.deleteUser);
