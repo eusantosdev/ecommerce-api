51a289de109240b47f9808e9e4aac198b39588a6
eusantosdev <eusantosdev@gmail.com>
2025-07-01 19:17:02 -0300

    feat: criação dos controllers

---
+++ src/controllers/users.controllers.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from 'express';
+
+const users = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+    }
+];
+
+type User = {
+    id: number;
+    name: string,
+    email: string
+}
+
+export class UsersControllers {
+    static getAllUsers = (req: Request, res: Response) => {
+        try {
+            res.status(201).send(users);
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            res.status(500).send({
+                message: `Internal Server Error`
+            });
+        }
+    }
+
+    static getUserById = (req: Request, res: Response) => {
+        try {
+            let { id } = req.params;
+            let user = users.find(user => user.id === parseInt(id));
+            res.send(user);
+        } catch (error) {
+            console.log(`Erro: ${error}`);
+            res.status(500).send({
+                message: `Internal Server Error`
+            });
+        }
+    }
+
+    static createUser = (req: Request, res: Response) => {
+        try {
+            let { name, email } = req.body as User;
+
+            const newUser = {
+                id: users.length + 1,
+                name,
+                email
+            }
+
+            users.push(newUser);
+
+        res.status(201).send({
+            message: `Usuario ${newUser.id} criado!`,
+        });
+        } catch (error) {
+            console.log(`Erro ao criar o usuario ${error}`);
+            res.status(500).send({
+                message: 'Erro ao criar o usuario'
+            });
+        }
+    }
+}
+
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,67 +1,11 @@
 import express, { Request, Response } from 'express';
+import { UsersControllers } from '../controllers/users.controllers';
 
 export const userRoutes = express.Router();
 
-const users = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'john.doe@example.com'
-    }
-];
-
-type User = {
-    id: number;
-    name: string,
-    email: string
-}
-
-userRoutes.get('/users', (req: Request, res: Response) => {
-    try {
-        res.status(201).send(users);
-    } catch (error) {
-        console.log(`Error: ${error}`);
-        res.status(500).send({
-            message: `Internal Server Error`
-        });
-    }
-});
-
-userRoutes.get('/users/:id', (req: Request, res: Response) => {
-    try {
-        let { id } = req.params;
-        let user = users.find(user => user.id === parseInt(id));
-        res.send(user);
-    } catch (error) {
-        console.log(`Erro: ${error}`);
-        res.status(500).send({
-            message: `Internal Server Error`
-        });
-    }
-});
-
-userRoutes.post('/users', (req: Request, res: Response) => {
-    try {
-        let { name, email } = req.body as User;
-
-        const newUser = {
-            id: users.length + 1,
-            name,
-            email
-        }
-
-        users.push(newUser);
-
-        res.status(201).send({
-            message: `Usuario ${newUser.id} criado!`,
-        });
-    } catch (error) {
-        console.log(`Erro ao criar o usuario ${error}`);
-        res.status(500).send({
-            message: 'Erro ao criar o usuario'
-        });
-    }
-});
+userRoutes.get('/users', UsersControllers.getAllUsers);
+userRoutes.get('/users/:id', UsersControllers.getUserById);
+userRoutes.post('/users', UsersControllers.createUser);
 
 userRoutes.put('/users/:id', (req: Request, res: Response) => {
     try {
